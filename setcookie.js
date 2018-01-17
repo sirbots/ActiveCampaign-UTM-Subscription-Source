cfe6c173f28cfb73e6d38c1aacbe7f7e89ba22e3
Robert Carter <rcarter@activecampaign.com>
2018-01-17 16:42:32 -0600

    Update setcookie.js

--- setcookie.js
+++ setcookie.js
@@ -1,38 +1,36 @@
 
 
 
-  function getQueryVariable(variable) {
-    var query = window.location.search.substring(1);
-    var vars = query.split("&");
-    for (var i=0;i<vars.length;i++) {
-      var pair = vars[i].split("=");
-        if(pair[0] == variable) {
-          return pair[1];
-        }
-    }
-    
-    return(false);
-    console.log(pair);
+function getQueryVariable(variable) {
+  var query = window.location.search.substring(1);
+  var vars = query.split("&");
+  for (var i=0;i<vars.length;i++) {
+    var pair = vars[i].split("=");
+      if(pair[0] == variable) {
+        return pair[1];
+      }
   }
+  return(false);
+
+}
+
+var medium = getQueryVariable("utm_medium");
+var source = getQueryVariable("utm_medium");
+var campaign = getQueryVariable("utm_campaign");
+var content = getQueryVariable("utm_content");
+var term = getQueryVariable("utm_term");
 
 
-/*
-function setCookie(UTMstring) {
-
-
+function setCookie() {
   var expirationDate = new Date();
   expirationDate.setMonth(expirationDate.getMonth() + 1);
   expirationString = expirationDate.toUTCString;
 
+  document.cookie = "UTMparams=" + medium + "/" + source + "/" + campaign + "/" + content + "/" + term + ";expires=" + expirationString + ";path=/;"
 
-  
+}
 
-
-    
-    
-    document.cookie =   "UTMparams=[UTM-STUFF];expires=" + expirationString + ";path=/;"
-    
-*/
+setCookie();
 
 
   //  When you are creating a cookie, you can set six parts: 
